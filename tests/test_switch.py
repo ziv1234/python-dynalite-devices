9838a9e9009fc0763e2231aae75a6808dacd811c
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-03-04 22:09:59 +0000

    tests for switch

--- tests/test_switch.py
+++ tests/test_switch.py
@@ -22,10 +22,7 @@ async def test_switch(mock_gw):
                             dyn_const.CONF_NAME: preset_name,
                             dyn_const.CONF_FADE: 0.5,
                         },
-                        "4": {
-                            dyn_const.CONF_NAME: preset_name,
-                            dyn_const.CONF_FADE: 0.7,
-                        },
+                        "4": {dyn_const.CONF_FADE: 0.7},
                     },
                 }
             },
@@ -33,32 +30,33 @@ async def test_switch(mock_gw):
     )
     await mock_gw.async_setup()
     mock_gw.new_dev_func.assert_called_once()
-    assert len(mock_gw.new_dev_func.mock_calls[0][1][0]) == 1
-    device = mock_gw.new_dev_func.mock_calls[0][1][0][0]
-    assert device.category == "switch"
-    await device.async_turn_on()
+    assert len(mock_gw.new_dev_func.mock_calls[0][1][0]) == 2
+    device1 = mock_gw.new_dev_func.mock_calls[0][1][0][0]
+    device4 = mock_gw.new_dev_func.mock_calls[0][1][0][1]
+    assert device1.category == "switch"
+    assert device4.category == "switch"
+    await device1.async_turn_on()
     await asyncio.sleep(0.1)
     mock_gw.mock_writer.write.assert_called_once()
     assert (
-        call.write(dyn_dynet.DynetPacket.set_channel_level_packet(1, 1, 1.0, 0.5).msg)
+        call.write(dyn_dynet.DynetPacket.select_area_preset_packet(1, 1, 0.5).msg)
         in mock_gw.mock_writer.mock_calls
     )
-    assert device.brightness == 255
+    assert device1.is_on
+    assert not device4.is_on
     mock_gw.mock_writer.reset_mock()
-    await device.async_turn_on(brightness=51)
+    await device4.async_turn_on()
     await asyncio.sleep(0.1)
     mock_gw.mock_writer.write.assert_called_once()
     assert (
-        call.write(dyn_dynet.DynetPacket.set_channel_level_packet(1, 1, 0.2, 0.5).msg)
+        call.write(dyn_dynet.DynetPacket.select_area_preset_packet(1, 4, 0.7).msg)
         in mock_gw.mock_writer.mock_calls
     )
-    assert device.brightness == 51
+    assert device4.is_on
+    assert not device1.is_on
     mock_gw.mock_writer.reset_mock()
-    await device.async_turn_off()
+    await device4.async_turn_off()
     await asyncio.sleep(0.1)
-    mock_gw.mock_writer.write.assert_called_once()
-    assert (
-        call.write(dyn_dynet.DynetPacket.set_channel_level_packet(1, 1, 0, 0.5).msg)
-        in mock_gw.mock_writer.mock_calls
-    )
-    assert device.brightness == 0
+    mock_gw.mock_writer.write.assert_not_called()
+    assert not device4.is_on
+    assert not device1.is_on
