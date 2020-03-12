127473b6a648d11e1f1d5dc3bac0d8e62197a787
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-03-12 20:20:41 +0000

    typing

--- dynalite_devices_lib/cover.py
+++ dynalite_devices_lib/cover.py
@@ -1,5 +1,6 @@
 """Support for the Dynalite channels as covers."""
 import asyncio
+from typing import Any
 
 from .const import ATTR_POSITION, ATTR_TILT_POSITION, CONF_TEMPLATE, CONF_TIME_COVER
 from .dynalitebase import DynaliteMultiDevice
@@ -8,39 +9,39 @@ from .dynalitebase import DynaliteMultiDevice
 class DynaliteTimeCoverDevice(DynaliteMultiDevice):
     """Representation of a Dynalite Channel as a Home Assistant Cover."""
 
-    def __init__(self, area, bridge, poll_timer):
+    def __init__(self, area: int, bridge: Any, poll_timer: float) -> None:
         """Initialize the cover."""
-        self._current_position = 0
+        self._current_position = 0.0
         self._direction = "stop"
         self._poll_timer = poll_timer
         super().__init__(4, area, bridge)
 
     @property
-    def available(self):
+    def available(self) -> bool:
         """Return if device is available."""
         return self._bridge.available(CONF_TEMPLATE, self._area, CONF_TIME_COVER)
 
     @property
-    def category(self):
+    def category(self) -> str:
         """Return the category of the entity: light, switch, or cover."""
         return "cover"
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         """Return the ID of this room switch."""
         return "dynalite_area_" + str(self._area) + "_time_cover"
 
     @property
-    def has_tilt(self):
+    def has_tilt(self) -> bool:
         """Return whether cover supports tilt."""
         return False
 
     @property
-    def device_class(self):
+    def device_class(self) -> str:
         """Return the class of the cover."""
         return self._bridge.get_device_class(self._area)
 
-    def update_level(self, actual_level, target_level):
+    def update_level(self, actual_level: float, target_level: float) -> None:
         """Update the current level."""
         if actual_level == target_level:
             self._direction = "stop"
@@ -50,7 +51,7 @@ class DynaliteTimeCoverDevice(DynaliteMultiDevice):
             self._bridge.add_timer_listener(self.timer_callback)
         self._bridge.update_device(self)
 
-    def timer_callback(self):
+    def timer_callback(self) -> None:
         """Update the progress of open and close."""
         duration = self._bridge.get_cover_duration(self._area)
         assert self._direction in ["open", "close"]
@@ -72,38 +73,38 @@ class DynaliteTimeCoverDevice(DynaliteMultiDevice):
         self._bridge.update_device(self)
 
     @property
-    def current_cover_position(self):
+    def current_cover_position(self) -> int:
         """Return the position of the cover from 0 to 100."""
         return int(self._current_position * 100)
 
     @property
-    def is_opening(self):
+    def is_opening(self) -> bool:
         """Return whether cover is currently opening."""
         return self._direction == "open"
 
     @property
-    def is_closing(self):
+    def is_closing(self) -> bool:
         """Return whether cover is currently closing."""
         return self._direction == "close"
 
     @property
-    def is_closed(self):
+    def is_closed(self) -> bool:
         """Return whether cover is closed."""
         return self._current_position == 0
 
-    async def async_open_cover(self, **kwargs):
+    async def async_open_cover(self, **kwargs) -> None:
         """Open the cover."""
         # pylint: disable=unused-argument
         await self.get_device(1).async_turn_on()
         self.update_level(self._current_position, 1.0)
 
-    async def async_close_cover(self, **kwargs):
+    async def async_close_cover(self, **kwargs) -> None:
         """Close the cover."""
         # pylint: disable=unused-argument
         await self.get_device(2).async_turn_on()
         self.update_level(self._current_position, 0.0)
 
-    async def async_set_cover_position(self, **kwargs):
+    async def async_set_cover_position(self, **kwargs) -> None:
         """Set the cover to a specific position."""
         target_position = kwargs[ATTR_POSITION] / 100
         position_diff = target_position - self._current_position
@@ -130,13 +131,14 @@ class DynaliteTimeCoverDevice(DynaliteMultiDevice):
         else:
             await self.async_stop_cover()
 
-    async def async_stop_cover(self, **kwargs):
+    async def async_stop_cover(self, **kwargs) -> None:
         """Stop the cover."""
         # pylint: disable=unused-argument
         await self.get_device(3).async_turn_on()
         self.update_level(self._current_position, self._current_position)
 
-    def listener(self, device, stop_fade):
+    # def listener(self, device: Union[DynalitePresetSwitchDevice, DynaliteChannelLightDevice], stop_fade: bool) -> None:
+    def listener(self, device: Any, stop_fade: bool) -> None:
         """Update according to updates in underlying devices."""
         if device == self.get_device(1):
             if device.is_on:
@@ -161,17 +163,17 @@ class DynaliteTimeCoverDevice(DynaliteMultiDevice):
 class DynaliteTimeCoverWithTiltDevice(DynaliteTimeCoverDevice):
     """Representation of a Dynalite Channel as a Home Assistant Cover that uses up and down for tilt."""
 
-    def __init__(self, area, bridge, poll_timer):
+    def __init__(self, area: int, bridge: Any, poll_timer: float) -> None:
         """Initialize the cover."""
         self._current_tilt = 0
         super().__init__(area, bridge, poll_timer)
 
     @property
-    def has_tilt(self):
+    def has_tilt(self) -> bool:
         """Return whether cover supports tilt."""
         return True
 
-    def update_tilt(self, poll_timer):
+    def update_tilt(self, poll_timer: float) -> None:
         """Update the current tilt based on diff and tilt_percentage."""
         assert self._direction in ["open", "close"]
         if self._direction == "open":
@@ -184,11 +186,11 @@ class DynaliteTimeCoverWithTiltDevice(DynaliteTimeCoverDevice):
         self._current_tilt = max(0, min(1, self._current_tilt + tilt_diff))
 
     @property
-    def current_cover_tilt_position(self):
+    def current_cover_tilt_position(self) -> int:
         """Return the current cover tilt."""
         return int(self._current_tilt * 100)
 
-    async def apply_tilt_diff(self, tilt_diff):
+    async def apply_tilt_diff(self, tilt_diff: float) -> None:
         """Move the cover up or down based on a diff."""
         duration = self._bridge.get_cover_duration(self._area)
         tilt_duration = self._bridge.get_cover_tilt_duration(self._area)
@@ -199,26 +201,26 @@ class DynaliteTimeCoverWithTiltDevice(DynaliteTimeCoverDevice):
         )
         await self.async_set_cover_position(position=target_position)
 
-    async def async_open_cover_tilt(self, **kwargs):
+    async def async_open_cover_tilt(self, **kwargs) -> None:
         """Open the cover tilt."""
         # pylint: disable=unused-argument
         if self._current_tilt == 1:
             return
         await self.apply_tilt_diff(1 - self._current_tilt)
 
-    async def async_close_cover_tilt(self, **kwargs):
+    async def async_close_cover_tilt(self, **kwargs) -> None:
         """Close the cover tilt."""
         # pylint: disable=unused-argument
         if self._current_tilt == 0:
             return
         await self.apply_tilt_diff(0 - self._current_tilt)
 
-    async def async_set_cover_tilt_position(self, **kwargs):
+    async def async_set_cover_tilt_position(self, **kwargs) -> None:
         """Set the cover tilt position."""
         target_position = kwargs[ATTR_TILT_POSITION] / 100
         await self.apply_tilt_diff(target_position - self._current_tilt)
 
-    async def async_stop_cover_tilt(self, **kwargs):
+    async def async_stop_cover_tilt(self, **kwargs) -> None:
         """Stop cover tilt."""
         # pylint: disable=unused-argument
         await self.async_stop_cover()
