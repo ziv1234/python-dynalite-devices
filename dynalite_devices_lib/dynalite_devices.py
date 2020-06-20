a7eb460615d936edb5f81e425dc353a0a10eb813
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-06-20 23:52:24 +0300

    vscode settings formatting

--- dynalite_devices_lib/dynalite_devices.py
+++ dynalite_devices_lib/dynalite_devices.py
@@ -114,7 +114,10 @@ class DynaliteDevices:
                 self._dynalite.request_area_preset(area)
             for channel in self._area[area][CONF_CHANNEL]:
                 self.create_channel_if_new(area, channel)
-                if CONF_TEMPLATE not in self._area[area] or self._area[area][CONF_TEMPLATE] != CONF_TIME_COVER:
+                if (
+                    CONF_TEMPLATE not in self._area[area]
+                    or self._area[area][CONF_TEMPLATE] != CONF_TIME_COVER
+                ):
                     if self._active in [ACTIVE_INIT, ACTIVE_ON]:
                         self._dynalite.request_channel_level(area, channel)
             for preset in self._area[area][CONF_PRESET]:
